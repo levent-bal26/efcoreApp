4d71f39519fe632d453860f627c2a2e3e25e9813
LEVENT-BAL <119902197+LEVENT-BAL@users.noreply.github.com>
2025-10-07 16:13:35 +0300

    Dördüncü Commit

--- obj/Debug/net9.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a628c4773ffed81bc0c632c566fab0e16f0f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8599666e4e96e3df364f3d412ff447fcaf2a8c77")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
